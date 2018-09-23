c4eb6d0e123d623095c380fc207cfe2bab5e5753
yaowalak.j <yaowalak.j@hotmail.com>
2018-09-24 01:53:14 +0700

    add element

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Link from './Link'
+import '../Css/Footer.css';
 
 class Footer extends React.Component {
     render() {
-        return <h1>Footer</h1>
-        
+        return (
+            <React.Fragment>
+                <div className="footer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-6 col-sm-3">
+                                <Link title="Quis incididunt1" link="Id culpa incididunt"  link="Id culpa incididunt"/>
+                            </div>
+                            <div className="col-xs-6 col-sm-3">
+                                <Link title="Quis incididunt2" link="Id culpa incididunt" />
+                            </div>
+                            <div className="col-xs-6 col-sm-3">
+                                <Link title="Quis incididunt3" link="Id culpa incididunt" />
+                            </div>
+                            <div className="col-xs-12 col-sm-3">
+                                <Link title="Quis incididunt4"/>
+                                <img src="..." alt="..." className="img-circle"/>
+                                <img src="..." alt="..." className="img-circle"/>
+                                <img src="..." alt="..." className="img-circle"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
     }
 }
 
