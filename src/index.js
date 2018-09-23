4bc83141c00f719310bf679b8de02ab9ffd36de9
yaowalak.j <yaowalak.j@hotmail.com>
2018-09-24 00:43:01 +0700

    add render Header, Content and Footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Content />, document.getElementById('content'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 registerServiceWorker();
