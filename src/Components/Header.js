af7bb753d11d60959c88cfe7cbe0cfbdff740fb8
yaowalak.j <yaowalak.j@hotmail.com>
2018-09-24 01:30:17 +0700

    add img and nav tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../Css/Header.css';
 
 class Header extends React.Component {
     render() {
-        return <h1>Header</h1>
-        
+        return (
+            <React.Fragment>
+                <div className="container header-data">
+                    <img src="..." alt="..." className="img-circle"/>
+                    <ul className="nav">
+                        <li className="active"><a href="#">Dashboard</a></li>
+                        <li><a href="#">Inbox</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Refer Friends</a></li>
+                    </ul>
+                </div>
+            </React.Fragment>
+        )   
     }
 }
 
