a76715a3df545d04c3e764591260b84c70be1e07
yaowalak.j <yaowalak.j@hotmail.com>
2018-09-24 01:30:59 +0700

    add info element

---
+++ src/Components/Info.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Info extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="info-data">
+                    <h2><strong>Anim elit ex ullamco amet et.</strong></h2>
+                    <p>In tempor Lorem eu minim in esse duis mollit. Laborum velit exercitation tempor aliqua tempor minim magna aute sit proident incididunt est Lorem officia.</p>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Info
