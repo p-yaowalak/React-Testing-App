81d42e514241b6ea3f49173cc82b4500865ec7a0
yaowalak.j <yaowalak.j@hotmail.com>
2018-09-26 00:11:46 +0700

    remove element and use elenment form bootstrap instead

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import '../Css/Header.css';
 
+
 class Header extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <div className="container header-data">
-                    <img src="..." alt="..." className="img-circle"/>
-                    <ul className="nav">
-                        <li className="active"><a href="#">Dashboard</a></li>
-                        <li><a href="#">Inbox</a></li>
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#">Refer Friends</a></li>
+             
+                    <div className="logo">
+                        <img src="..." alt="..." className="img-circle"/>
+                    </div>
+                    <ul className="nav nav-pills">
+                        <li className="active"><a href="#"><strong>Home</strong></a></li>
+                        <li><a href="#"><strong>About Us</strong></a></li>
+                        <li><a href="#"><strong>Gallery</strong></a></li>
+                        <li><a href="#"><strong>Contact</strong></a></li>
                     </ul>
                 </div>
             </React.Fragment>
